5744da02611bddd18d05ca7804cf12893ce4dd0a
Logan <15618309240@163.com>
2015-12-26 15:46:24 +0800

    car-buy-index-header

--- mewchan/ui/1jia2/header/car-buy-index-header/script.js
+++ mewchan/ui/1jia2/header/car-buy-index-header/script.js
@@ -3,7 +3,19 @@ function goBack(page){
         "action": "backward"
     });
 }
+
+var upDownS = function(){
+	if($(this.element).hasClass("up")){
+		$(this.element).removeClass("up");
+		return;
+	}
+	$(this.element).addClass("up");
+
+	$(".header-optionBox").slideToggle();
+}
+
 // 暴露函数
-module.exports = { 
-   "goBack" : goBack
+module.exports = {
+   "goBack" : goBack,
+	 "upDownS": upDownS
 };
