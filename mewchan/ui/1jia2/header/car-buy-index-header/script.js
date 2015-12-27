3a78a2633b8f92e0a49cfb3d9d51c5be5c6540b8
Logan <15618309240@163.com>
2015-12-27 12:44:37 +0800

    car-buy-index-header

--- mewchan/ui/1jia2/header/car-buy-index-header/script.js
+++ mewchan/ui/1jia2/header/car-buy-index-header/script.js
@@ -5,13 +5,45 @@ function goBack(page){
 }
 
 var upDownS = function(){
+	//遍历是否有动画
+	var animated = false;
+	$(".header-nav p").find(".header-optionBox").each(function(){
+		if($(this).is(":animated")){
+			animated = true;
+			return;
+		}
+	});
+	if(animated){return;}
+
+	// 如果点击了打开的按钮
 	if($(this.element).hasClass("up")){
+		$(".header-nav p").find(".header-optionBox").each(function(){
+			if(parseInt($(this).css("height") ) > 0){
+				$(this).animate({ height : "0px"})
+			}
+		});
 		$(this.element).removeClass("up");
 		return;
 	}
-	$(this.element).addClass("up");
 
-	$(".header-optionBox").slideToggle();
+	//如果点击非打开的按钮
+	$(".header-nav p").each(function(){
+		if($(this).hasClass("up")){
+			$(this).removeClass("up");
+		}
+	})
+
+	$(".header-nav p").find(".header-optionBox").each(function(){
+		if(parseInt($(this).css("height") ) > 0){
+			$(this).animate({ height : "0px"})
+		}
+	});
+	$(this.element).find(".header-optionBox").animate({
+		height : "300px"
+	});
+
+
+	$(this.element).addClass("up");
 }
 
 // 暴露函数
