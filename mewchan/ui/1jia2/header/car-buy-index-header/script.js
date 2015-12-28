1a7c7b8520d556f554e802917161e14b775cff13
Logan <15618309240@163.com>
2015-12-28 09:02:39 +0800

    car-buy-index-header over

--- mewchan/ui/1jia2/header/car-buy-index-header/script.js
+++ mewchan/ui/1jia2/header/car-buy-index-header/script.js
@@ -5,9 +5,27 @@ function goBack(page){
 }
 
 var upDownS = function(){
+	var order = $(this.element).attr("order");
+	var hg = parseInt($(window).height()) - 108;
+	// switch (order)
+	// {
+	// 	case "1":
+	// 	  hg = 221;
+	// 	  break;
+	// 	case "2":
+	// 		hg = 352;
+	// 		break;
+	// 	case "3":
+	// 		hg = parseInt($(window.height)) - 108;
+	// 		break;
+	// 	case "4":
+	// 		hg = 221;
+	// 		break;
+	// }
+
 	//遍历是否有动画
 	var animated = false;
-	$(".header-nav p").find(".header-optionBox").each(function(){
+	$(".header-nav-box p").find(".header-optionBox").each(function(){
 		if($(this).is(":animated")){
 			animated = true;
 			return;
@@ -17,9 +35,9 @@ var upDownS = function(){
 
 	// 如果点击了打开的按钮
 	if($(this.element).hasClass("up")){
-		$(".header-nav p").find(".header-optionBox").each(function(){
+		$(".header-nav-box p.header-nav").find(".header-optionBox").each(function(){
 			if(parseInt($(this).css("height") ) > 0){
-				$(this).animate({ height : "0px"})
+				$(this).animate({ height : "0px"},200)
 			}
 		});
 		$(this.element).removeClass("up");
@@ -27,27 +45,51 @@ var upDownS = function(){
 	}
 
 	//如果点击非打开的按钮
-	$(".header-nav p").each(function(){
+	$(".header-nav-box p.header-nav").each(function(){
 		if($(this).hasClass("up")){
 			$(this).removeClass("up");
 		}
-	})
+	});
 
-	$(".header-nav p").find(".header-optionBox").each(function(){
+	$(".header-nav-box p.header-nav").find(".header-optionBox").each(function(){
 		if(parseInt($(this).css("height") ) > 0){
-			$(this).animate({ height : "0px"})
+			$(this).animate({ height : "0px"},200)
 		}
 	});
+	console.log(hg);
 	$(this.element).find(".header-optionBox").animate({
-		height : "300px"
-	});
+		height : hg
+	},200);
 
 
 	$(this.element).addClass("up");
 }
 
+var checkFil = function(){
+	//去除事件冒泡
+	(function(event){
+		 e = event || window.event;
+		 if(e.stopPropagation){
+			 e.stopPropagation();
+		  }else{
+				e.cancelBubble = true;
+			}
+		})();
+
+	/*点击选中*/
+	//先遍历去除
+	$(".header-optionBox p.header-filterK").each(function(){
+		if($(this).hasClass("checked")){
+			$(this).removeClass("checked");
+		}
+	});
+	//选中addClass
+	$(this.element).addClass("checked");
+}
+
 // 暴露函数
 module.exports = {
-   "goBack" : goBack,
-	 "upDownS": upDownS
+   "goBack"  : goBack,
+	 "upDownS" : upDownS,
+	 "checkFil": checkFil
 };
